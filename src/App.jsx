81492b2eb06507037011a74d04f6afdf82a593f6
f3xlqz <f3xlqz1212312121@gmail.com>
2025-12-05 20:33:01 +0700

    add page

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import SearchResults from './pages/SearchResults';  // เพิ่มบรรทัดนี้
+import SearchResults from './pages/SearchResults';
+import PropertyDetail from './pages/PropertyDetail';
 
 function App() {
     return (
@@ -14,6 +15,7 @@ function App() {
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/search-results" element={<SearchResults />} />
+                <Route path="/property/:id" element={<PropertyDetail />} />
             </Routes>
         </BrowserRouter>
     );
